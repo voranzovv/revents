4b3bb2160fc8207f604a8c4643dbaecaafe14e05
voranzovv <voranzov@gmail.com>
2019-10-16 22:56:38 +1100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Sentry.init({dsn: "https://1f8e44542b1948f5be2d9ad2e15edf6f@sentry.io/1781488"});
+
+const rootEl = document.getElementById('root');
+
+let render=()=>{
+    ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot){
+    module.hot.accept('./App', ()=>{
+        setTimeout(render)
+    })
+}
+
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
