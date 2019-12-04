b13b2215b0a57b4a4c3cdb5e88395d3470e2f776
voranzovv <voranzov@gmail.com>
2019-12-04 22:38:36 +1100

    Updates

---
+++ precache-manifest.f1167055632c5b40965ceca7b2320e3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6743111e1954d94aad0c",
+    "url": "/revents/static/css/main.1d8511e7.chunk.css"
+  },
+  {
+    "revision": "6743111e1954d94aad0c",
+    "url": "/revents/static/js/main.cdfb990b.chunk.js"
+  },
+  {
+    "revision": "ddc71e1dc3765257341f",
+    "url": "/revents/static/js/runtime~main.feadbf73.js"
+  },
+  {
+    "revision": "034ee017b59a74d50e26",
+    "url": "/revents/static/js/2.4afad6c1.chunk.js"
+  },
+  {
+    "revision": "046405a1748244649f0ab919709beaea",
+    "url": "/revents/index.html"
+  }
+];
